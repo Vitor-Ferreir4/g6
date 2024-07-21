1ee2878ab3327264e773968e215c6d170c082261
Vitor-Ferreir4 <jesusecristo13@gmail.com>
2024-07-21 16:52:02 -0300

    primeira att 21/07

---
+++ src/JS/Login.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import '../CSS/Login.css';
+import { Link } from 'react-router-dom';
+
+const Login = () => {
+  return (
+    <div className="container">
+      <div className="login-box">
+        <h1 className="title">Login</h1>
+        <form className="form">
+          <input type="text" className="input" placeholder="Username" />
+          <input type="password" className="input" placeholder="Password" />
+          <button className="button">Login</button>
+        </form>
+        <div className="links">
+          <a href="#" className="link">Esqueceu a senha?</a>
+          <Link to='/cadastro' className="link">Cadastrar-se</Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
---
+++ src/JS/Register.js
@@ -0,0 +1,84 @@
+import React, { useState } from 'react';
+import '../CSS/Register.css';
+
+const Register = () => {
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [errors, setErrors] = useState({});
+
+  const validate = () => {
+    const newErrors = {};
+
+    if (!username) newErrors.username = 'Preencha esse campo.';
+    if (!email.includes('@') || !email.includes('.com')) newErrors.email = 'Email inválido.';
+    if (!password) {
+      newErrors.password = 'Preencha esse campo.';
+    } else if (password.length < 8) {
+      newErrors.password = 'A senha deve ter no mínimo 8 caracteres.';
+    } else if (!/[A-Z]/.test(password)) {
+      newErrors.password = 'A senha deve ter pelo menos uma letra maiúscula.';
+    } else if (!/[a-z]/.test(password)) {
+      newErrors.password = 'A senha deve ter pelo menos uma letra minúscula.';
+    } else if (!/[0-9]/.test(password)) {
+      newErrors.password = 'A senha deve ter pelo menos um número.';
+    } else if (!/[!@#$%^&*]/.test(password)) {
+      newErrors.password = 'A senha deve ter pelo menos um caractere especial.';
+    }
+
+    setErrors(newErrors);
+    return Object.keys(newErrors).length === 0;
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (validate()) {
+      // Process the form submission here
+      console.log('Formulário enviado com sucesso!');
+      // Limpar os campos após o envio bem-sucedido
+      setUsername('');
+      setEmail('');
+      setPassword('');
+    }
+  };
+
+  return (
+    <div className="container">
+      <div className="register-box">
+        <h1 className="title">Cadastro</h1>
+        <form className="form" onSubmit={handleSubmit}>
+          <input
+            type="text"
+            className="input"
+            placeholder="Username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          {errors.username && <p className="error">{errors.username}</p>}
+          
+          <input
+            type="email"
+            className="input"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          {errors.email && <p className="error">{errors.email}</p>}
+          
+          <input
+            type="password"
+            className="input"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          {errors.password && <p className="error">{errors.password}</p>}
+          
+          <button className="button" type="submit">Cadastrar</button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Register;
